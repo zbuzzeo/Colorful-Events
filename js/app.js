e750c0f2e2db0fb9a835f8fc69599c18b2a15a65
victorlhlee <victorlhlee@users.noreply.github.com>
2018-06-20 17:37:54 -1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,1 @@
-var mainElem = document.getElementById("main");
 
-mainElem.addEventListener("click", showColor);
-
-function showColor(){
-  var trueElem = document.getElementById("true");
-  var alphaNum = "0123456789ABCDEF";
-  var color = "#";
-  for(var i = 0; i<6; i++){
-    color += alphaNum[Math.floor(Math.random()* alphaNum.length)];
-    
-  }
-  
-  console.log(color);
-  trueElem.style.backgroundColor = color;
-  
-   
-}
