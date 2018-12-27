43ca3a42174fecfd0d9e09bd55c8e1069dc61e3a
Zekiel Buzzeo <zeke.buzzeo@gmail.com>
2018-12-26 17:03:52 -1000

    complete

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 
+const getColorButton = document.getElementById ( 'clickMe' );
+
+const randomNumber = ( ) => {
+        const generateNumber = Math.random ( ) * Math.ceil ( 255 );
+        if ( generateNumber < 125 ) {
+                getColorButton.style.color = 'white';
+
+        } else {
+                getColorButton.style.color = 'buttontext';      
+                
+        }
+        return generateNumber;
+
+};
+
+getColorButton.addEventListener ( 'click', ( ) => {
+        // alert ( `Color changed! 😱`);
+        getColorButton.style.backgroundColor = `rgba( ${ randomNumber ( ) }, ${ randomNumber ( ) }, ${ randomNumber ( ) } )`;
+
+} );
